cc1cf84d310784d1086fff002aed104e4dfbbec6
Luzions <148138608+Luzions@users.noreply.github.com>
2023-10-19 12:04:34 -0400

    Update Lab6-VC.py

--- Lab6-VC.py
+++ Lab6-VC.py
@@ -1,3 +1,4 @@
+#Michael Sanchez 
 def encode(password):
     new_encode = list(str(password))
     new_encode = [int(i)+3 or int('0') == 3 for i in new_encode]         # compressed list incrementing each item. Added accountability for '0'
